2f34453702c542d4e4b9281f2380430977459138
Danish <dan71ish@gmail.com>
2024-04-06 16:59:36 +0530

    remove previous comments on the code

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -12,56 +12,6 @@ const Hero = () => {
     <>
       {visible ? <HamMenus fn={setVisible} /> : ""}
 
-      {/* <div className="h-auto md:min-h-[95vh] bg-[#5BD7E8] border-bottom">
-        <div className="flex">
-          <div className="flex-1">
-            <img src={logo} className="w-[220px] ml-[6rem]" alt="" />
-            <div>
-              <div className="flex flex-col md:flex-row items-center justify-center max-w-[1800px] mx-auto">
-                <div className="text relative tilt">
-                  <h1 className="text-[#4700FF] uppercase font-bold rubik text-2xl md:text-8xl tracking-0 my-4">
-                    make your
-                  </h1>
-                  <h2 className="uppercase rubik text-8xl text-white add-shadow my-2 ml-40">
-                    brand
-                    <img
-                      src={soundGif}
-                      className="absolute right-[20%] top-12 w-40"
-                      alt=""
-                    />
-                  </h2>
-                  <h3 className="ml-32 uppercase text-7xl text-[#ff007f] rubik">
-                    standout
-                  </h3>
-                </div>
-                <div>
-                  <img src={heroImg} className="w-full md:w-[800px]" alt="" />
-                </div>
-              </div>
-              <div className="relative">
-                <h1 className="text-[#4A0292] kanit text-4xl font-semibold text-center relative z-20">
-                  Unleast your Brand Power
-                </h1>
-                <img
-                  src={strokeImg}
-                  className="absolute top-[-150%] left-[50%] w-[300px] z-10"
-                  alt=""
-                />
-              </div>
-            </div>
-          </div>
-
-          <div className="border-l-4   px-8 border-[#FF007F] h-auto md:h-[95vh] flex items-center justify-center">
-            <button
-              className="text-4xl text-[#FF007F]"
-              onClick={() => setVisible((prev) => !prev)}
-            >
-              &#9776;
-            </button>
-          </div>
-        </div>
-      </div> */}
-
       <div className="bg-[#5bd7e8]">
         <div className="flex border-b-4 border-black">
           <div className="content flex-1">
@@ -78,7 +28,11 @@ const Hero = () => {
                     brand
                   </h2>
 
-                  <img src={soundGif} className="w-64 absolute bottom-[2rem] right-[-9rem]" alt="" />
+                  <img
+                    src={soundGif}
+                    className="w-64 absolute bottom-[2rem] right-[-9rem]"
+                    alt=""
+                  />
                 </div>
 
                 <h3 className="uppercase text-8xl text-[#ff007f] rubik text-right">
@@ -104,7 +58,7 @@ const Hero = () => {
             </div>
           </div>
 
-          <div className="border-l-4 border-[#FF007F] flex items-center justify-center px-4">
+          <div className="border-l-4 border-[#FF007F] flex items-center justify-center px-8">
             <button
               className="text-4xl text-[#FF007F]"
               onClick={() => setVisible((prev) => !prev)}
@@ -112,7 +66,6 @@ const Hero = () => {
               &#9776;
             </button>
           </div>
-          
         </div>
 
         <div className="headings flex justify-around text-3xl uppercase rubik tracking-wider py-6 border-b-4 border-black italic">
