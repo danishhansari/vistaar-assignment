5275bf5a6c65877bab15dbdd21df01230d456519
Danish <dan71ish@gmail.com>
2024-04-06 10:18:21 +0530

    basic styling done

--- src/components/MetricMeasure.jsx
+++ src/components/MetricMeasure.jsx
@@ -40,33 +40,108 @@ const data = [
 ];
 
 const MetricMeasure = () => {
-  const [currentIndex, setCurrentIndex] = useState(1);
+  const [currentIndex, setCurrentIndex] = useState(0);
   return (
     <>
-      <div className="flex gap-4">
+      <div className="flex gap-8 h-[50vh] md:h-[80vh]">
         {currentIndex > 0 && (
-          <div>
-            <img src={data[currentIndex - 1].gif} alt="Paragraph" />
-            <h1>{data[currentIndex - 1].heading}</h1>
-            <p>{data[currentIndex - 1].text}</p>
+          <div className="bg-[#9E04C5] p-6 rounded-3xl ">
+            <div
+              className={
+                currentIndex - 1 === 4
+                  ? "unique-border-2 flex items-center justify-center"
+                  : currentIndex - 1 === 1 ||
+                    currentIndex - 1 === 3 ||
+                    currentIndex - 1 === 5
+                  ? "unique-border flex items-center justify-center"
+                  : "flex items-center justify-center"
+              }
+            >
+              <img
+                src={data[currentIndex - 1].gif}
+                className="max-w-[180px] w-full"
+                alt="Paragraph"
+              />
+            </div>
+            <h1 className="rotate-[-90deg] text-sm md:text-2xl text-white translate-x-0 translate-y-[100px] md:translate-y-[180px] kanik font-bold text-center">
+              {data[currentIndex - 1].heading}
+            </h1>
           </div>
         )}
-        <div className="bg-red-400">
-          <img src={data[currentIndex].gif} alt="Paragraph" />
-          <h1>{data[currentIndex].heading}</h1>
-          <p>{data[currentIndex].text}</p>
+        <div className="bg-[#9E04C5] p-6 rounded-3xl flex flex-col justify-center items-center h-full">
+          <div
+            className={
+              currentIndex === 4
+                ? "unique-border-2 flex items-center justify-center max-w-[380px]"
+                : currentIndex === 1 || currentIndex === 5 || currentIndex === 3
+                ? "unique-border flex items-center justify-center max-w-[380px]"
+                : "flex items-center justify-center"
+            }
+          >
+            <img src={data[currentIndex].gif} alt="Paragraph" />
+          </div>
+          <h1 className="kanik text-4xl font-bold text-white my-4">
+            {data[currentIndex].heading}
+          </h1>
+          <p className="max-w-[400px] w-full text-center kanik text-xl font-bold text-white">
+            {data[currentIndex].text}
+          </p>
         </div>
+
         {currentIndex < data.length - 1 && (
-          <div>
-            <img src={data[currentIndex + 1].gif} alt="Paragraph" />
-            <h1>{data[currentIndex + 1].heading}</h1>
-            <p>{data[currentIndex + 1].text}</p>
+          <div className="bg-[#9E04C5] p-6 rounded-3xl">
+            <div
+              className={
+                currentIndex + 1 === 4
+                  ? "unique-border-2 flex items-center justify-center"
+                  : currentIndex + 1 === 1 ||
+                    currentIndex + 1 === 3 ||
+                    currentIndex + 1 === 5
+                  ? "unique-border flex items-center justify-center"
+                  : "flex items-center justify-center"
+              }
+            >
+              <img
+                src={data[currentIndex + 1].gif}
+                className="max-w-[180px] w-full"
+                alt="Paragraph"
+              />
+            </div>
+            <h1 className="rotate-[-90deg] text-sm md:text-2xl text-white translate-x-0 translate-y-[100px] md:translate-y-[200px] kanik font-bold text-center">
+              {data[currentIndex + 1].heading}
+            </h1>
+          </div>
+        )}
+
+        {currentIndex === 0 && (
+          <div className="bg-[#9E04C5] p-6 rounded-3xl">
+            <div
+              className={
+                currentIndex + 1 === 1 ||
+                currentIndex + 1 === 4 ||
+                currentIndex + 1 === 3 ||
+                currentIndex + 1 === 5
+                  ? "unique-border flex items-center justify-center"
+                  : "flex items-center justify-center"
+              }
+            >
+              <img
+                src={data[currentIndex + 2].gif}
+                className="max-w-[180px] w-full"
+                alt="Paragraph"
+              />
+            </div>
+            <h1 className="rotate-[-90deg] text-sm md:text-2xl text-white translate-x-0 translate-y-[100px] md:translate-y-[200px] kanik font-bold text-center">
+              {data[currentIndex + 2].heading}
+            </h1>
           </div>
         )}
       </div>
+
       {currentIndex === data.length - 1 && (
-        <button onClick={() => setCurrentIndex(1)}>Go Back</button>
+        <button onClick={() => setCurrentIndex(0)}>Go Back</button>
       )}
+
       {currentIndex < data.length - 1 && (
         <button
           onClick={() =>
