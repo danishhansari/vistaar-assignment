bff48bcf4a50b9c2290d09b4d3935912f30cd14e
Danish <dan71ish@gmail.com>
2024-04-06 17:13:46 +0530

    minor tweaks

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -17,25 +17,26 @@ const Hero = () => {
           <div className="content flex-1">
             <img src={logo} className="max-w-[200px] w-full" alt="Logo" />
 
-            <div className="flex justify-between px-4">
+            <div className="flex justify-around px-4 items-center">
+
               <div className="text tilt ml-28">
-                <h1 className="text-[#4700FF] uppercase font-bold rubik text-2xl md:text-8xl tracking-0 leading-none text-right">
+                <h1 className="text-[#4700FF] uppercase font-bold rubik text-2xl md:text-6xl tracking-0 leading-none text-right">
                   make your
                 </h1>
 
                 <div className="relative">
-                  <h2 className="uppercase rubik text-[11rem] text-white add-shadow text-right leading-none">
+                  <h2 className="uppercase rubik text-[9rem] text-white add-shadow text-right leading-none">
                     brand
                   </h2>
 
                   <img
                     src={soundGif}
-                    className="w-64 absolute bottom-[2rem] right-[-9rem]"
+                    className="w-48 absolute bottom-[2rem] right-[-8rem]"
                     alt=""
                   />
                 </div>
 
-                <h3 className="uppercase text-8xl text-[#ff007f] rubik text-right">
+                <h3 className="uppercase text-6xl text-[#ff007f] rubik text-right">
                   standout
                 </h3>
               </div>
@@ -46,7 +47,7 @@ const Hero = () => {
             </div>
 
             <div className="relative h-[150px] flex justify-between items-center">
-              <h1 className="text-[#4A0292] kanit text-5xl mx-auto font-semibold text-center italic relative z-20">
+              <h1 className="text-[#4A0292] kanit text-4xl mx-auto font-semibold text-center italic relative z-20">
                 Unleast your Brand Power
               </h1>
               <img
@@ -54,7 +55,7 @@ const Hero = () => {
                 className="max-w-[300px] w-full stroke-img"
                 alt=""
               />
-              <img src={curlLine} className="w-48 curl-line" alt="line" />
+              <img src={curlLine} className="w-40 curl-line" alt="line" />
             </div>
           </div>
 
@@ -68,7 +69,7 @@ const Hero = () => {
           </div>
         </div>
 
-        <div className="headings flex justify-around text-3xl uppercase rubik tracking-wider py-6 border-b-4 border-black italic">
+        <div className="headings flex gap-[2rem] justify-around text-2xl uppercase rubik tracking-wider py-6 border-b-4 border-black italic">
           <h1 className="text-[#9E04C5] move">drive sales</h1>
           <h1 className="text-[#4700FF] move">grab attention</h1>
           <h1 className="text-[#FF018F] move">ignite buzz</h1>
--- src/pages/CampaignIdeasPage.jsx
+++ src/pages/CampaignIdeasPage.jsx
@@ -1,5 +1,5 @@
 import logoLight from "../assets/logo-light.jpg";
-import campaignIdeas1 from "../assets/campaignIdeas1.png"
+import campaignIdeas1 from "../assets/campaignIdeas1.png";
 
 const CampaignIdeasPage = () => {
   return (
@@ -13,9 +13,10 @@ const CampaignIdeasPage = () => {
         </div>
 
         <div className="flex px-4 w-full items-center justify-between">
-
           <div>
-            <h1 className="kanik mt-4 text-6xl font-semibold text-[#4700FF]">Photo Activity (Pictivatr)</h1>
+            <h1 className="kanik mt-4 text-6xl font-semibold text-[#4700FF]">
+              Photo Activity (Pictivatr)
+            </h1>
             <p className="text-xl w-[50%] tracking-wide leading-8">
               Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
               eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut
@@ -26,9 +27,84 @@ const CampaignIdeasPage = () => {
           </div>
 
           <div>
-            <img className="w-[1850px] mr-[-12rem]" src={campaignIdeas1} alt="" />
+            <img
+              className="w-[1850px] mr-[-12rem]"
+              src={campaignIdeas1}
+              alt=""
+            />
           </div>
         </div>
+
+        <div className="flex px-4 w-full items-center justify-between">
+          <div>
+            <h1 className="kanik mt-4 text-6xl font-semibold text-[#4700FF]">
+              Photo Activity (Pictivatr)
+            </h1>
+            <p className="text-xl w-[50%] tracking-wide leading-8">
+              Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+              eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut
+              enim ad minim veniam, quis nostrud exercitation ullamco laboris
+              nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in
+              reprehenderit in voluptate velit esse cillum dolore{" "}
+            </p>
+          </div>
+
+          <div>
+            <img
+              className="w-[1850px] mr-[-12rem]"
+              src={campaignIdeas1}
+              alt=""
+            />
+          </div>
+        </div>
+
+        <div className="flex px-4 w-full items-center justify-between">
+          <div>
+            <h1 className="kanik mt-4 text-6xl font-semibold text-[#4700FF]">
+              Photo Activity (Pictivatr)
+            </h1>
+            <p className="text-xl w-[50%] tracking-wide leading-8">
+              Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+              eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut
+              enim ad minim veniam, quis nostrud exercitation ullamco laboris
+              nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in
+              reprehenderit in voluptate velit esse cillum dolore{" "}
+            </p>
+          </div>
+
+          <div>
+            <img
+              className="w-[1850px] mr-[-12rem]"
+              src={campaignIdeas1}
+              alt=""
+            />
+          </div>
+        </div>
+
+        <div className="flex px-4 w-full items-center justify-between">
+          <div>
+            <h1 className="kanik mt-4 text-6xl font-semibold text-[#4700FF]">
+              Photo Activity (Pictivatr)
+            </h1>
+            <p className="text-xl w-[50%] tracking-wide leading-8">
+              Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+              eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut
+              enim ad minim veniam, quis nostrud exercitation ullamco laboris
+              nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in
+              reprehenderit in voluptate velit esse cillum dolore{" "}
+            </p>
+          </div>
+
+          <div>
+            <img
+              className="w-[1850px] mr-[-12rem]"
+              src={campaignIdeas1}
+              alt=""
+            />
+          </div>
+        </div>
+
+        
       </div>
     </>
   );
